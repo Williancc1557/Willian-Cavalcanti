bb5ad779fda1053187c161b5389d4d1e791024f2
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-10-25 22:44:12 -0300

    feat: show random cactus between small and large

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -3,7 +3,7 @@ import random
 from dino_runner.components.obstacles.obstacles import Obstacle
 
 class Cactus(Obstacle):
-    def __init__(self, image):
+    def __init__(self, image, vertical_position=325):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        self.rect.y = vertical_position
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,7 @@
 import pygame
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
+from random import randint
 
 class ObstacleManager:
     def __init__(self):
@@ -8,7 +9,15 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            cactus_images = [SMALL_CACTUS, LARGE_CACTUS]
+            index = randint(0, 1)
+            cactus_image_random = cactus_images[index]
+            cactus = Cactus(cactus_image_random)
+
+            if index == 1:
+                cactus = Cactus(cactus_image_random, 300)
+
+            self.obstacles.append(cactus)
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
