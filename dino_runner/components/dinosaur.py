8d0a71be06e8f68d5abc7c2c158fb5d4595a2d55
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-10-25 21:35:44 -0300

    feat: make obstacles and ensure Dino when collide to stop the game

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
