c7718117145a39275d675b636cf389bff06b00c9
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-10-26 21:57:20 -0300

    feat: add menu and display an death page when lose

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -30,8 +30,40 @@ class ObstacleManager:
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
+
+
+"""- mostrar uma mensagem de restart (por exemplo "Press any key to restart")
+
+ - mostrar pontuação atingida
+
+ - mostrar o contador de mortes
+ 
+
+> Resetar a contagem de pontuação e a velocidade, cada vez que o jogo é "restartado"
+
+
+> Remover a repetição de código pra formatação de texto em "draw_score()" e "show_menu()"
+
+ >> arquivo "dino_runner\components\game.py"
+
+  >>> draw_score() linhas 82 a 86
+
+  >>> show_menu() linhas 102 a 106
+ 
+
+ - dica: talvez criar um utilitário para formatar textos, 
+
+  função q pudesse ser reutilizada pra editar todos textos, mudando algumas variáveis tipo: 
+
+   - texto a ser exibido 
+
+   - posição do texto"""
